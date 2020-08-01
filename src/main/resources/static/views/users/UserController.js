e27b5bb5fde217df89936a81dbcc4869e5c3cb09
Denys Azhymov <azhimov93@gmail.com>
2020-08-01 23:46:15 +0300

    amend add create user logic

--- src/main/resources/static/views/users/UserController.js
+++ src/main/resources/static/views/users/UserController.js
@@ -1,14 +1,20 @@
 angular.module('smetnerdemo')
-    .controller('UserController', function ($scope, $rootScope, User, $location) {
+    .controller('UserController', function ($scope, $rootScope, User, $location, $window) {
 
         $scope.createUser = function () {
             User.addUser($scope.user, function (success) {
+                $rootScope.newUserId = success.id;
                 $location.path("/")
             }, function (error) {
                 $scope.error = true;
                 console.log(error);
                 $scope.errorMessage = error.status;
             });
+        };
+
+        $scope.cancel = function () {
+            $rootScope.newUserId = null;
+            $window.history.back()
         };
 
         $scope.submit = function () {
